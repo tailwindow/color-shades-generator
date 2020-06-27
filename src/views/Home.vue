ece0e42576996f4fa9071348c6217124e4e0e970
Martien Dermawan Tanama <martiendt@gmail.com>
2020-06-27 21:30:16 +0700

    add color range function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,26 +24,32 @@
             Color shades generator
           </p>
         </div>
-        <div class="mt-8 sm:mt-0">
-          <label for="regular" class="text-xs leading-7 text-gray-700 font-bold uppercase">Color Interval</label>
-          <input id="regular" v-model="colorInterval" type="text" class="shadow w-full border bg-gray-200 border-gray-400 py-1 px-4 text-gray-700 text-md focus:outline-none focus:border-gray-500" placeholder="Regular input text">
+        <div class="w-64 mt-8 sm:mt-0 flex">
+          <div class="flex flex-col mr-4 text-center">
+            <label for="regular" class="text-xs leading-4 text-gray-700 font-bold uppercase">Color Interval</label> <span class="text-xs font-hairline">( max 100 )</span>
+            <input id="regular" v-model="colorInterval" type="number" class="shadow w-full mt-1 border bg-gray-200 border-gray-400 py-1 px-4 text-gray-700 text-md focus:outline-none focus:border-gray-500" placeholder="Input percentage color interval">
+          </div>
+          <div class="flex flex-col mr-4 text-center">
+            <label for="regular" class="text-xs leading-4 text-gray-700 font-bold uppercase">Color Range</label> <span class="text-xs font-hairline">( 2 - 9 )</span>
+            <input id="regular" v-model="colorRange" type="number" class="shadow w-full mt-1 border bg-gray-200 border-gray-400 py-1 px-4 text-gray-700 text-md focus:outline-none focus:border-gray-500" placeholder="Input percentage color interval">
+          </div>
         </div>
       </div>
-      <hr class="border-gray-400 mx-4 mt-4">
+      <hr class="border-gray-400 mx-4">
       <div class="flex w-full sm:flex-row flex-wrap justify-center">
-        <div v-for="(color, index) in colors" :key="color" class="px-4 w-1/2 sm:w-1/3 md:w-1/4">
+        <div v-for="(color, index) in colors" :key="index" class="px-4 w-1/2 sm:w-1/3 md:w-1/5">
           <div class="flex mt-8 mb-4">
             <div class="shadow-md w-8 text-center text-xs py-2 bg-gray-300">
               #
             </div>
             <input
               id="color"
-              :value="color"
+              v-model="color.hex"
               type="text"
               class="shadow-md bg-gray-200 w-full text-sm p-2 outline-none"
               name="color"
               placeholder="HEX COLOR"
-              @keyup.enter="updateColor($event, index)"
+              @keyup="updateColor($event, index)"
             >
           </div>
           <div class="flex flex-col mb-8">
@@ -54,7 +60,7 @@
               :class="{ 'text-white' : !isLumaLight(shade) }"
               :style="{ backgroundColor: '#' + shade }"
             >
-              <p class="text-md py-4 px-4 select-all text-center uppercase opacity-0 hover:opacity-75">
+              <p class="text-md py-4 px-4 select-all text-center uppercase hover:opacity-75">
                 #{{ shade }}
               </p>
             </div>
@@ -73,27 +79,57 @@ export default {
   name: 'Home',
   data () {
     return {
-      colors: ['3366FF', '7FED25', '2AC4FC', 'FF5028'],
-      colorInterval: 0.18, // percentage
-      colorShades: []
+      colors: [
+        { hex: '3F51B5' },
+        { hex: '4CAF50' },
+        { hex: '2196F3' },
+        { hex: 'FFEB3B' },
+        { hex: 'F44336' }
+      ],
+      colorRange: 4, // (2 - 9)
+      colorInterval: 20, // percentage (max 100)
+      interval: 0.20, // percentage (max 1)
+      colorShades: [],
+      beta: [{
+        hex: ''
+      }]
     }
   },
   watch: {
     colorInterval () {
+      if (this.colorInterval > 100) {
+        this.colorInterval = 100
+      }
+      if (this.colorInterval < 0) {
+        this.colorInterval = 0
+      }
+      this.interval = this.colorInterval / 100
       this.colors.forEach((color, index) => {
-        this.createTintsAndShades(color, index)
+        this.createTintsAndShades(color.hex, index)
+      })
+    },
+    colorRange () {
+      if (this.colorRange > 9) {
+        this.colorRange = 9
+      }
+      if (this.colorRange < 2) {
+        this.colorRange = 2
+      }
+      this.colors.forEach((color, index) => {
+        this.createTintsAndShades(color.hex, index)
       })
     }
   },
   created () {
     this.colors.forEach((color, index) => {
-      this.createTintsAndShades(color, index)
+      this.createTintsAndShades(color.hex, index)
     })
   },
   methods: {
     updateColor (e, index) {
-      this.createTintsAndShades(e.target.value, index)
-      this.colors[index] = e.target.value
+      if (e.target.value.length === 6) {
+        this.createTintsAndShades(e.target.value, index)
+      }
     },
     isLumaLight (color) {
       var c = color.replace('#', '')
@@ -130,16 +166,16 @@ export default {
     },
     rgbShade (rgb, i) {
       return {
-        red: rgb.red * (1 - this.colorInterval * i),
-        green: rgb.green * (1 - this.colorInterval * i),
-        blue: rgb.blue * (1 - this.colorInterval * i)
+        red: rgb.red * (1 - this.interval * i),
+        green: rgb.green * (1 - this.interval * i),
+        blue: rgb.blue * (1 - this.interval * i)
       }
     },
     rgbTint (rgb, i) {
       return {
-        red: rgb.red + (255 - rgb.red) * i * this.colorInterval,
-        green: rgb.green + (255 - rgb.green) * i * this.colorInterval,
-        blue: rgb.blue + (255 - rgb.blue) * i * this.colorInterval
+        red: rgb.red + (255 - rgb.red) * i * this.interval,
+        green: rgb.green + (255 - rgb.green) * i * this.interval,
+        blue: rgb.blue + (255 - rgb.blue) * i * this.interval
       }
     },
     calculateShades (colorValue) {
@@ -151,8 +187,9 @@ export default {
     calculate (colorValue, shadeOrTint) {
       var color = this.hexToRGB(colorValue)
       var shadeValues = []
-      for (var i = 1; i < 5; i++) {
-        shadeValues[i - 1] = this.rgbToHex(shadeOrTint(color, i))
+      const range = this.colorRange
+      for (var i = 0; i < range; i++) {
+        shadeValues[i] = this.rgbToHex(shadeOrTint(color, i + 1))
       }
       return shadeValues
     },
